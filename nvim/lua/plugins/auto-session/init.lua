2fbb2094b7c0be0e9f1400416857f61f83eb4ae5
Mike Duminy <michael-james.duminy@klarna.com>
2022-11-27 23:49:12 +0100

    attempt to get auto-session working

--- nvim/lua/plugins/auto-session/init.lua
+++ nvim/lua/plugins/auto-session/init.lua
@@ -1,6 +1,10 @@
 local plugin = require('auto-session')
 
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 plugin.setup({
+  log_level = 'error',
+  auto_session_enabled = true,
   auto_session_suppress_dirs = { "~/", "~/Klarna", "~/source", "~/Downloads", "/" },
   cwd_change_handling = {
     post_cwd_changed_hook = function()
