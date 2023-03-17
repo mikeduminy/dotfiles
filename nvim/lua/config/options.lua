a25be1ad91c7aca962a0114ecd4a10a2c024bf27
Mike Duminy <michael-james.duminy@klarna.com>
2023-03-17 12:47:54 +0100

    format nvim options and switch to jumplist stack behaviour

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,11 +7,25 @@ vim.opt.fillchars = ""
 
 -- goto file (gf) settings
 _G.resolve_module = require("utils.resolve_module")
-vim.opt.path = ".,src" -- goto file (gf) should search these paths
-vim.opt.suffixesadd = ".js,.jsx,.ts,.tsx"
-vim.opt.includeexpr = "v:lua.resolve_module(v:fname)" -- goto file (gf) on a package reference runs this command
-vim.opt.isfname = "@,48-57,/,.,-,_,+,,,#,$,%,~,=,@-@" -- default file name pattern with added @-@ to match scoped packages
 
+-- goto file (gf) should search these paths
+vim.opt.path = ".,src"
+vim.opt.suffixesadd = ".js,.jsx,.ts,.tsx"
+
+-- goto file (gf) on a package reference runs this command
+vim.opt.includeexpr = "v:lua.resolve_module(v:fname)"
+
+-- default file name pattern with added @-@ to match scoped packages
+vim.opt.isfname = "@,48-57,/,.,-,_,+,,,#,$,%,~,=,@-@"
+
+-- larger bottom scroll offset
 vim.opt.scrolloff = 12
-vim.opt.textwidth = 80 -- Line wrap (number of columns)
-vim.opt.shell = "/bin/zsh" -- https://superuser.com/questions/287994/how-to-specify-shell-for-vim
+
+-- Line wrap (number of columns)
+vim.opt.textwidth = 80
+
+-- https://superuser.com/questions/287994/how-to-specify-shell-for-vim
+vim.opt.shell = "/bin/zsh"
+
+-- use a tagstack/web-style jumplist navigation
+vim.opt.jumpoptions = "stack"
