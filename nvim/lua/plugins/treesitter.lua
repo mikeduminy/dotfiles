618fcb30908bdb87eebca83fbd2763d7a017bdff
Mike Duminy <michael-james.duminy@klarna.com>
2023-06-27 10:46:12 +0200

    [nvim] fix incremental selection, bump deps

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -12,14 +12,8 @@ return {
     end
     opts.highlight.disable = is_large_file
     opts.indent.disable = is_large_file
-    opts.incremental_selection = is_large_file
+    opts.incremental_selection.disable = is_large_file
 
     return opts
   end,
-  -- keys = function(_, _keys)
-  --   -- a bug in treesitter might be causing a crash due to default keys in
-  --   -- LazyVim so we need to remove these key bindings
-  --   -- https://github.com/nvim-treesitter/nvim-treesitter/issues/4000
-  --   return {}
-  -- end,
 }
