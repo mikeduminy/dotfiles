8f5324199df0838390eb6c028c911784c7cd2efc
Mike Duminy <michael-james.duminy@klarna.com>
2023-08-23 17:07:59 +0200

    [nvim] move lazy to packager

--- nvim/lua/packager.lua
+++ nvim/lua/packager.lua
@@ -13,10 +13,10 @@ require("lazy").setup({
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
     { import = "lazyvim.plugins.extras.util.project" },
     -- import/override with your plugins
     { import = "plugins" },
