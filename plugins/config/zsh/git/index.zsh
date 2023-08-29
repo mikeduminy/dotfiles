716c282b5306c240958f85c1067e4eb0a3506234
Mike Duminy <michael-james.duminy@klarna.com>
2023-08-29 16:19:34 +0200

    [zsh] add rebase self alias

--- plugins/config/zsh/git/index.zsh
+++ plugins/config/zsh/git/index.zsh
@@ -3,12 +3,21 @@ if ! type __git_prompt_git > /dev/null; then
     source $(dirname $0)/oh-my-git.zsh
 fi
 
-## Replacement aliases
-unalias grbm # oh-my-zsh sets this
+# Rebase current branch onto the latest main
+unalias grbm # oh-my-zsh git plugin sets this
 alias grbm='gfa; git rebase origin/$(git_main_branch)'
-alias gmt='gmtl' # git plugin replaced gmt with gmtl
+
+# Open merge tool
+alias gmt='gmtl' # oh-my-zsh git plugin replaced gmt with gmtl
+
+# Open diff tool
 alias gdt='git difftool'
 
+# Interactively rebase the current branch onto the common ancestor of the current branch and the main branch
+alias grbis='git rebase -i `git merge-base \`current_branch\` origin/$(git_main_branch)`'
+
+# Fetch all remotes and prune deleted branches
+alias gfa='git fetch --tags --force && git fetch --all --prune --jobs=10'
 
 ## Copy latest commit hash to clipboard
 alias gtop='git log -1 --format="%H" | cat | xargs echo -n | pbcopy'
