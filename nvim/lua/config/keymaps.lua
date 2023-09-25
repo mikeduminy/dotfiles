ea356b6790b555fd08415eb86ac9a15e06cc5fcf
Mike Duminy <michael-james.duminy@klarna.com>
2023-09-25 15:44:17 +0200

    [nvim] vendor splits keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,10 +2,23 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set
-
 local file_utils = require("utils.file")
 local git_utils = require("utils.git")
+
+-- vendored from lazyvim https://github.com/LazyVim/LazyVim/blob/f9dadc11b39fb0b027473caaab2200b35c9f0c8b/lua/lazyvim/config/keymaps.lua
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
 
 -----------------------------------------------------------------------
 -- Keymaps
@@ -31,6 +44,14 @@ end
 
 -- hack for better saving using CMD+s
 map("n", withModifier("s"), ":write", { desc = "Save buffer", remap = true })
+
+-- splits
+map("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
+map("n", "<leader>w_", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>w|", "<C-W>v", { desc = "Split window right", remap = true })
+map("n", "<leader>_", "<C-W>s", { desc = "Split window below", remap = true })
+map("n", "<leader>|", "<C-W>v", { desc = "Split window right", remap = true })
 
 -- navigating between windows
 map("n", withModifier("h"), "<C-w>h", { desc = "Go to left window", remap = true })
