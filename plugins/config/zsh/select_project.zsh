0dff1c79356f1311828ca6c496a149bf8984601c
Mike Duminy <michael-james.duminy@klarna.com>
2023-12-06 11:45:02 +0100

    add git branch to project selector

--- plugins/config/zsh/select_project.zsh
+++ plugins/config/zsh/select_project.zsh
@@ -10,15 +10,38 @@ source $XDG_CONFIG_HOME/wezterm/api.zsh
 project_roots=(${(s/:/)PROJECT_ROOTS})
 
 # 1. Get all project folders without trailing slash
-project_folders=$(fd --min-depth 1 --max-depth 1 -t directory . ${project_roots[@]} | xargs realpath)
+project_folders=($(fd --min-depth 1 --max-depth 1 -t directory . ${project_roots[@]} | xargs realpath))
 
 # 1.1. Add any additional project folders we don't want to expand
 project_folders+=(${(s/:/)PROJECT_FOLDERS})
 
+# 2. Construct a list of project folders and their git branch
+selection_list=()
+for folder in "${project_folders[@]}"; do
+  # change to project folder
+  cd $folder || continue
+
+  # shorten folder path
+  local short_folder=$(echo $folder | sed "s|$HOME/||")
+
+  # get git branch
+  branch=$(git symbolic-ref --short HEAD 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    selection_list+=("$short_folder [$branch]")
+  else
+    selection_list+=("$short_folder")
+  fi
+
+  # return to previous folder
+  cd - > /dev/null
+done
+
 # 2. Select a project using fzf (all paths are relative to $HOME)
-selected_folder=$(printf '%s\n' "${project_folders[@]}" | sed "s|$HOME/||" | fzf --layout reverse --height 50%)
+selected_item=$(printf '%s\n' "${selection_list[@]}" | fzf --layout reverse --height 50%)
 local retval=$?
 if (($retval == 0)); then
+  # 2.1. Get the selected folder from the selected item
+  selected_folder=$(echo $selected_item | awk '{print $1}')
 	# 3. Switch to the workspace by communicating with wezterm
 	wezterm-switch-workspace "$selected_folder" "$HOME/$selected_folder"
 else
