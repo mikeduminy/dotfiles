21ab662dc3d775f13a63a7dc6f9eef5339aeff5c
Mike Duminy <michael-james.duminy@klarna.com>
2023-12-07 11:36:17 +0100

    [nvim] disable neo-tree, hide '..' in oil

--- nvim/lua/plugins/file-tree.lua
+++ nvim/lua/plugins/file-tree.lua
@@ -1,51 +1,7 @@
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
-    keys = {
-      -- swap e and E
-      { "<leader>E", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
-      { "<leader>e", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
-      -- {
-      --   "<BS>",
-      --   function()
-      --     require("neo-tree.command").execute({ position = "current", reveal = true, dir = vim.loop.cwd() })
-      --   end,
-      --   desc = "Open NeoTree in current window",
-      --   remap = true,
-      -- },
-    },
-    init = function()
-      vim.g.neo_tree_remove_legacy_commands = 1
-    end,
-    opts = {
-      filesystem = {
-        filtered_items = { hide_dotfiles = false, hide_gitignored = false },
-        follow_current_file = { enabled = true },
-        -- hijack_netrw_behavior = "open_current",
-        bind_to_cwd = true,
-        cwd_target = { current = "tab" },
-      },
-      window = {
-        mappings = {
-          -- disable fuzzy finder (allow vi text search)
-          ["/"] = "noop",
-          -- disable help (allow vi text search)
-          ["?"] = "noop",
-
-          -- remap for help
-          ["h"] = "show_help",
-
-          -- disable split keys (allow leap motions)
-          ["s"] = "noop",
-          ["S"] = "noop",
-
-          -- remap split keys
-          ["sh"] = "open_split",
-          ["sv"] = "open_vsplit",
-        },
-        position = "left",
-      },
-    },
+    enable = false,
   },
   {
     "stevearc/oil.nvim",
@@ -68,7 +24,6 @@ return {
         ["<CR>"] = "actions.select",
         ["<Leader>_"] = "actions.select_split",
         ["<Leader>|"] = "actions.select_vsplit",
-        ["<esc>"] = "actions.close",
         ["?"] = "actions.show_help",
         ["R"] = "actions.refresh",
         ["_"] = "actions.open_cwd",
@@ -78,6 +33,9 @@ return {
       view_options = {
         -- Show files and directories that start with "."
         show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          return (name == "..")
+        end,
       },
     },
     keys = {
