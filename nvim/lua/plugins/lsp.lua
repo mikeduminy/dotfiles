e99042e4f28ae93d733910f58f4d590d6d70e77a
Mike Duminy <mjduminy@gmail.com>
2024-02-28 12:03:21 +0100

    [nvim] add ts-error-translator

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,32 +1,35 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      tsserver = {
-        init_options = {
-          -- support large TS projects
-          maxTsServerMemory = 8192,
-          -- disableAutomaticTypingAcquisition = true,
-        },
-        preferences = {
-          includeCompletionsForModuleExports = false,
-          includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tsserver = {
+          init_options = {
+            -- support large TS projects
+            maxTsServerMemory = 8192,
+            -- disableAutomaticTypingAcquisition = true,
+          },
+          preferences = {
+            includeCompletionsForModuleExports = false,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+          },
         },
       },
-    },
-    setup = {
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "tsserver" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
-      -- eslint = function(_, opts)
-      --   opts.capabilities.documentFormattingProvider = false
-      -- end,
+      setup = {
+        eslint = function()
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+        -- eslint = function(_, opts)
+        --   opts.capabilities.documentFormattingProvider = false
+        -- end,
+      },
     },
   },
+  { "dmmulroy/ts-error-translator.nvim" },
 }
