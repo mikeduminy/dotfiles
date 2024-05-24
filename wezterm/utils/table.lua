c41a1e72948733640a754a132bcdf2837900a6c9
Mike Duminy <mjduminy@gmail.com>
2024-05-24 10:48:09 +0200

    wezterm merge tables

--- wezterm/utils/table.lua
+++ wezterm/utils/table.lua
@@ -1,19 +1,37 @@
 local module = {}
 
---- @param a table
---- @param b table
-function module.mergeValues(a, b)
-  local mergedTable = {}
+--- @param a string
+--- @param b string
+local function sort_alphabetical(a, b)
+  return a:lower() < b:lower()
+end
 
-  for _, v in pairs(a) do
-    table.insert(mergedTable, v)
+--- @param tblA table
+--- @param tblB table
+function module.mergeValues(tblA, tblB)
+  if not next(tblA) then
+    -- if a is empty, sort b and return it
+    table.sort(tblB, sort_alphabetical)
+    return tblB
   end
 
-  for _, v in pairs(b) do
-    table.insert(mergedTable, v)
+  -- otherwise, merge the tables into a single dictionary
+  local mergeDict = {}
+  for _, v in pairs(tblA) do
+    mergeDict[v] = v
+  end
+  for _, v in pairs(tblB) do
+    mergeDict[v] = v
   end
 
-  return mergedTable
+  -- convert to array and sort it alphabetically
+  local mergeArr = {}
+  for _, v in pairs(mergeDict) do
+    table.insert(mergeArr, v)
+  end
+
+  table.sort(mergeArr, sort_alphabetical)
+  return mergeArr
 end
 
 return module
