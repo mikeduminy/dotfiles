61721eee70ddd4730991eb219f9334ebf259737a
Mike Duminy <mjduminy@gmail.com>
2024-06-25 16:57:03 +0200

    [nvim] disable all treesitter modules on large files

--- nvim/lua/plugins/illuminate.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "RRethy/vim-illuminate",
-  opts = { large_file_cutoff = 50000 },
-}
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,42 @@
 local file = require("utils.file")
 
+local max_file_size = 512 * 1024 -- 512kb
+local function is_large_file(_lang, bufnr)
+  local bufsize = file.get_buf_size(bufnr)
+  if bufsize and bufsize > max_file_size then
+    local buf_name = vim.api.nvim_buf_get_name(bufnr)
+    vim.notify_once("File is too large to enable treesitter: " .. buf_name, vim.log.levels.WARN)
+    return true
+  end
+  return false
+end
+
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    local max_file_size = 1024 * 1024
-    local function is_large_file(_lang, bufnr)
-      local bufsize = file.get_buf_size(bufnr)
-      if bufsize and bufsize > max_file_size then
-        vim.notify("File is too large to enable treesitter", vim.log.levels.WARN)
-        return true
+  {
+    -- configured in lazyvim
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      local modules =
+        { "context_commentstring", "highlight", "indent", "incremental_selection", "autotag", "textobjects" }
+
+      for _, mod in ipairs(modules) do
+        if opts[mod] then
+          opts[mod].disable = is_large_file
+        end
       end
-      return false
-    end
 
-    if opts.context_commentstring then
-      opts.context_commentstring.disable = is_large_file
-    end
-    opts.highlight.disable = is_large_file
-    opts.indent.disable = is_large_file
-    opts.incremental_selection.disable = is_large_file
-
-    opts.ensure_installed = vim.tbl_extend("force", opts.ensure_installed, { "groovy" })
-    return opts
-  end,
+      opts.ensure_installed = vim.tbl_extend("force", opts.ensure_installed, { "groovy" })
+      return opts
+    end,
+  },
+  {
+    -- configured in lazyvim.plugins.extras.editor.illuminate
+    "RRethy/vim-illuminate",
+    opts = {
+      large_file_cutoff = 2000,
+      should_enable = function(bufnr)
+        return not is_large_file(nil, bufnr)
+      end,
+    },
+  },
 }
