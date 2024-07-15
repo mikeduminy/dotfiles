ef53d6fe4dec8bdc4c936f6e1606c34332f74ea2
Mike Duminy <mjduminy@gmail.com>
2024-07-15 16:41:35 +0200

    add comments to aliases

--- plugins/config/zsh/aliases.zsh
+++ plugins/config/zsh/aliases.zsh
@@ -7,18 +7,20 @@ source $(dirname $0)/git/index.zsh
 
 alias open_file_descriptors="sudo lsof -n | cut -f1 -d' ' | uniq -c | sort | tail"
 
+# editor
+alias vi="nvim" # use neovim as default editor
+alias vim="nvim"
+alias vih="nvim ." # open nvim in current directory
+
 alias lg="lazygit"
 # open lazygit focusing on a specific path
 alias lgf="lazygit --filter"
 
-alias vi="nvim"
-alias vim="nvim"
-alias vih="nvim ." # open nvim in current directory
-
+# project switching
 alias select-project=$(dirname $0)/select_project.zsh
 alias sp=select-project
 
-alias cd="z"
-alias cat="bat"
-alias ls="eza --all --group-directories-first --icons"
+alias cd="z" # z is a better cd (zoxcide)
+alias cat="bat" # bat is a better cat (sharkdp/bat)
+alias ls="eza --all --group-directories-first --icons" # eza is a better ls (eza-community/eza)
 alias procs="procs --load-config $XDG_CONFIG_HOME/procs/config.toml"
