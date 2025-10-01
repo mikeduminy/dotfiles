0632fdf68ffe3ac8f4d8f5e4c7bc685ce20f9cb0
Mike Duminy <mjduminy@gmail.com>
2025-10-01 13:23:12 +0200

    enable copilot at work

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    opts = function(_, opts)
-      return vim.tbl_deep_extend("force", opts, {
-        -- lazyvim extra auto-connects copilot to cmp for LSP servers
-        -- but these filetypes have no LSP servers
-        filetypes = {
-          zsh = true,
-        },
-      })
-    end,
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   opts = function(_, opts)
+  --     return vim.tbl_deep_extend("force", opts, {
+  --       -- lazyvim extra auto-connects copilot to cmp for LSP servers
+  --       -- but these filetypes have no LSP servers
+  --       filetypes = {
+  --         zsh = true,
+  --       },
+  --     })
+  --   end,
+  -- },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     opts = function(_, opts)
