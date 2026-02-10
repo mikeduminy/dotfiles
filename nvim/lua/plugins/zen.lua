1d9792367cb8ddf517d18915b90281270530de91
Mike Duminy <mjduminy@gmail.com>
2026-02-10 11:07:49 +0100

    switch zen plugins

---
+++ nvim/lua/plugins/zen.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      zen = {
+        enabled = false,
+      },
+    },
+  },
+  {
+    "sand4rt/zen.nvim",
+    lazy = false,
+  },
+}
