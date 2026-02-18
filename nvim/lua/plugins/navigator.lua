eea3679a6d92b6735c6922fb6c83b73cc95f9cf3
Mike Duminy <mjduminy@gmail.com>
2026-02-18 17:18:35 +0100

    enable tmux nvim navigation in ghostty

--- nvim/lua/plugins/navigator.lua
+++ nvim/lua/plugins/navigator.lua
@@ -1,36 +1,61 @@
 return {
-  "numToStr/Navigator.nvim",
-  event = "WinEnter",
-  keys = {
-    {
-      "<C-h>",
-      function()
-        require("Navigator").left()
-      end,
-      { desc = "Go to left window" },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
     },
-    {
-      "<C-j>",
-      function()
-        require("Navigator").down()
-      end,
-      { desc = "Go to lower window" },
-    },
-    {
-      "<C-k>",
-      function()
-        require("Navigator").up()
-      end,
-      { desc = "Go to upper window" },
-    },
-    {
-      "<C-l>",
-      function()
-        require("Navigator").right()
-      end,
-      { desc = "Go to right window" },
+    init = function()
+      -- don't wrap when navigating to panes with the cursor at the border
+      vim.g.tmux_navigator_no_wrap = 1
+    end,
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
     },
   },
-  config = true,
+
+  -- {
+  --   "numToStr/Navigator.nvim",
+  --   event = "WinEnter",
+  --   keys = {
+  --     {
+  --       "<C-h>",
+  --       function()
+  --         require("Navigator").left()
+  --       end,
+  --       { desc = "Go to left window" },
+  --     },
+  --     {
+  --       "<C-j>",
+  --       function()
+  --         require("Navigator").down()
+  --       end,
+  --       { desc = "Go to lower window" },
+  --     },
+  --     {
+  --       "<C-k>",
+  --       function()
+  --         require("Navigator").up()
+  --       end,
+  --       { desc = "Go to upper window" },
+  --     },
+  --     {
+  --       "<C-l>",
+  --       function()
+  --         require("Navigator").right()
+  --       end,
+  --       { desc = "Go to right window" },
+  --     },
+  --   },
+  --   config = true,
+  -- },
 }
 -- require('Navigator').previous()
