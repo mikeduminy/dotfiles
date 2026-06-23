696f6988ef11aa69bb0a514c4b7f97d966a27849
mike.duminy <mike.duminy@mollie.com>
2026-06-23 10:19:30 +0200

    disable diagnostics on markdown files

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -6,6 +6,14 @@ return {
     build = "cd app && yarn install",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
+
+      -- disable diagnostics on markdown files by default
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function()
+          vim.diagnostic.enable(false, { bufnr = 0 })
+        end,
+      })
     end,
     ft = { "markdown" },
     keys = {
